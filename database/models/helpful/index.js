dd6422530a8044635b122637fb448f3c3c259bb9
randallwstanford <randallwstanford@gmail.com>
2021-10-05 11:25:11 -0600

    Update schema.

--- database/models/helpful/index.js
+++
@@ -1,8 +0,0 @@
-const { client } = require('../../../server/');
-
-exports.helpful = (req, res) => {
-  const query = `UPDATE reviews SET helpfulness = helpfulness + 1 WHERE id = $1;`;
-  client.query(query, [req.params.review_id])
-    .then(response => res.sendStatus(200))
-    .catch(err => console.log(err))
-};
--- database/schema.sql
+++ database/schema.sql
@@ -2,6 +2,8 @@
 
 CREATE SCHEMA IF NOT EXISTS public;
 
+SET max_parallel_workers_per_gather TO 8;
+
 CREATE TABLE IF NOT EXISTS reviews(
   id SERIAL PRIMARY KEY,
   product_id int not null,
@@ -13,9 +15,29 @@ CREATE TABLE IF NOT EXISTS reviews(
   reported BOOLEAN DEFAULT FALSE,
   reviewer_name varchar(100) not null,
   reviewer_email varchar(50) not null,
-  response varchar(255) null,
+  response varchar(255) DEFAULT null,
   helpfulness int DEFAULT 0
 );
+
+
+-- Parallel Seq Scan on reviews reviews_1
+-- Parallel Seq Scan on reviews reviews_2
+-- Parallel Seq Scan on reviews reviews_3
+-- Parallel Seq Scan on characteristics
+-- Parallel Seq Scan on characteristic_reviews
+-- Parallel Seq Scan on reviews
+
+-- CREATE INDEX IF NOT EXISTS id_index ON characteristics (id);
+
+-- CREATE INDEX IF NOT EXISTS product_id_index ON characteristics (product_id);
+CREATE INDEX IF NOT EXISTS characteristics_id_index ON characteristic_reviews (characteristics_id);
+CREATE INDEX IF NOT EXISTS product_id_index ON reviews (product_id);
+CREATE INDEX IF NOT EXISTS recommend_index ON reviews (recommend);
+CREATE INDEX IF NOT EXISTS rating_index ON reviews (rating);
+
+SELECT setval('reviews_id_seq', (SELECT MAX(id) FROM reviews)+1);
+
+-- Alters reviews by converting epoch time (13 digit int) into timestamp
 
 -- ALTER TABLE reviews
 --   ALTER COLUMN review_date
@@ -24,33 +46,57 @@ CREATE TABLE IF NOT EXISTS reviews(
 --         :: INTERVAL;
 
 
-CREATE INDEX IF NOT EXISTS recommend_index ON reviews (recommend);
-CREATE INDEX IF NOT EXISTS rating_index ON reviews (rating);
-
 CREATE TABLE IF NOT EXISTS photos(
-  id int not null PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   review_id int not null,
   photo_url VARCHAR(255) not null
 );
 
+SELECT setval('photos_id_seq', (SELECT MAX(id) FROM photos)+1);
+
 CREATE TABLE IF NOT EXISTS characteristics(
-  id int not null PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   product_id int not null,
   name varchar(50) not null
 );
+SELECT setval('characteristics_id_seq', (SELECT MAX(id) FROM characteristics));
 
 CREATE TABLE IF NOT EXISTS characteristic_reviews(
-  id int not null PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   characteristics_id int not null,
   review_id int not null,
   value int not null
 );
 
+SELECT setval('characteristic_reviews_id_seq', (SELECT MAX(id) FROM characteristic_reviews)+1);
+
 CREATE TABLE IF NOT EXISTS aggs_char AS SELECT
 char.name, char.id, avg(char_review.value) AS value, product_id FROM characteristics AS char INNER JOIN
 characteristic_reviews AS char_review ON char.id = char_review.characteristics_id GROUP BY char.id;
 
--- COPY characteristic_reviews FROM '/Users/randystanford/__SDC/reviews/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
+-- \COPY characteristic_reviews FROM '/Users/randystanford/__SDC/reviews/characteristic_reviews.csv' DELIMITER ',' CSV HEADER;
 -- \COPY photos FROM '/Users/randystanford/__SDC/reviews/reviews_photos.csv' DELIMITER ',' CSV HEADER;
 -- \COPY characteristics FROM '/Users/randystanford/__SDC/reviews/characteristics.csv' DELIMITER ',' CSV HEADER;
 -- \COPY reviews FROM '/Users/randystanford/__SDC/reviews/reviews.csv' DELIMITER ',' CSV HEADER;
+
+
+/*
+SELECT json_build_object(char.name, json_build_object('id', char.id, 'value', avg(char_review.value))) FROM characteristics as char INNER JOIN characteristic_reviews AS char_review ON char.id = char_review.characteristics_id WHERE char.product_id = 5 GROUP BY char.id;
+
+
+SELECT json_build_object(
+  characteristics.name, json_build_object(
+    'id', characteristics.id,
+    'value', avg(char_review.value)
+  )
+)
+FROM characteristics
+INNER JOIN characteristic_reviews
+ON characteristic_reviews.id = characteristic_reviews.characteristics_id
+WHERE characteristics.product_id = 5
+GROUP BY characteristics.id;
+
+
+
+
+*/
