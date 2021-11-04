eb3c92479306a9546f6367b8b381f62f60d08bd4
randallwstanford <randallwstanford@gmail.com>
2021-11-03 22:21:52 -0600

    Comments out psql \copy command to import files into db.

--- connect.sh
+++ connect.sh
@@ -1,12 +1,15 @@
 case $1 in
   postgres)
-    scp  -i ../pem/sdc.pem ./init.sh ec2-user@ec2-3-17-150-202.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i ../pem/sdc.pem ec2-user@ec2-3-17-150-202.us-east-2.compute.amazonaws.com
+    scp -i ../pem/sdc.pem ./init.sh ec2-user@ec2-3-15-153-141.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i ../pem/sdc.pem ec2-user@ec2-3-15-153-141.us-east-2.compute.amazonaws.com
     ;;
   node)
-    scp  -i ../pem/sdc.pem ./init.sh ec2-user@ec2-18-119-128-103.us-east-2.compute.amazonaws.com:~/init.sh
-    ssh -i ../pem/sdc.pem ec2-user@ec2-18-119-128-103.us-east-2.compute.amazonaws.com
+    # scp -i ../pem/sdc.pem ./init.sh ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com:~/init.sh
+    ssh -i ../pem/sdc.pem ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com
     ;;
-
 esac
 
+# scp -i ../pem/sdc.pem ./reviews/characteristic_reviews.csv ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com:~/Ratings-and-Reviews/characteristic_reviews.csv
+# scp -i ../pem/sdc.pem ./reviews/characteristics.csv ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com:~/Ratings-and-Reviews/characteristics.csv
+# scp -i ../pem/sdc.pem ./reviews/reviews_photos.csv ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com:~/Ratings-and-Reviews/reviews_photos.csv
+# scp -i ../pem/sdc.pem ./reviews/reviews.csv ec2-user@ec2-3-135-204-176.us-east-2.compute.amazonaws.com:~/Ratings-and-Reviews/reviews.csv
